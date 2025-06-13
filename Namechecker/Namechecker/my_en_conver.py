6a6d4886fb3a449ca4cd4b484c081c154a17cdd0
koko58447 <nyeinchan5391@gmail.com>
2025-06-13 16:23:05 +0630

    13-6-2025

--- Namechecker/Namechecker/my_en_conver.py
+++ Namechecker/Namechecker/my_en_conver.py
@@ -17,48 +17,48 @@ async def load_mappings():
 
 
 async def main():
-    # mapped_names = ''
-    # input_text="တိုးအောင်"
+    mapped_names = ''
+    input_text="တိုးအောင်"
     mapping_mm, mapping_pali = await load_mappings()   
     
-    # word_dict=mapping_mm
+    word_dict=mapping_mm
 
-    
-    # # Step 1: Preprocess
-    # normalized_text = p.normalize_text(input_text)
-    # processed_text = p.preprocess(normalized_text)
-  
-    # word_list = processed_text.split(' ')
-    # mapped_names = ' '.join(p.get_map(word, word_dict) for word in word_list)
-    
-   
-    # input_code_point = ''.join(hex(ord(char)) for char in processed_text)
-    # output_text= ' '.join(word.capitalize() for word in mapped_names.split())
-    # #output_text = p.capitalize(mapped_names)
-
-    # print("result str : "+ output_text)
-
-    ###############################
-
-    mapped_names1 = ''
-    input_text="နန္ဓမာလာဘိဝံသ"  
-    
-    word_dict=mapping_pali
-    print("result word : "+ word_dict)
     
     # Step 1: Preprocess
-    normalized_text1 = p.normalize_text(input_text)
-    processed_text = p.preprocess(normalized_text1)
+    normalized_text = p.normalize_text(input_text)
+    processed_text = p.preprocess(normalized_text)
   
     word_list = processed_text.split(' ')
     mapped_names = ' '.join(p.get_map(word, word_dict) for word in word_list)
     
    
     input_code_point = ''.join(hex(ord(char)) for char in processed_text)
-    output_text1= ' '.join(word.capitalize() for word in mapped_names.split())
+    output_text= ' '.join(word.capitalize() for word in mapped_names.split())
     #output_text = p.capitalize(mapped_names)
 
-    print("result str : "+ output_text1)  
+    print("result str : "+ output_text)
+
+    ###############################
+
+    # mapped_names1 = ''
+    # input_text="နန္ဓမာလာဘိဝံသ"  
+    
+    # word_dict=mapping_pali
+    # print("result word : "+ word_dict)
+    
+    # # Step 1: Preprocess
+    # normalized_text1 = p.normalize_text(input_text)
+    # processed_text = p.preprocess(normalized_text1)
+  
+    # word_list = processed_text.split(' ')
+    # mapped_names = ' '.join(p.get_map(word, word_dict) for word in word_list)
+    
+   
+    # input_code_point = ''.join(hex(ord(char)) for char in processed_text)
+    # output_text1= ' '.join(word.capitalize() for word in mapped_names.split())
+    # #output_text = p.capitalize(mapped_names)
+
+    # print("result str : "+ output_text1)  
 if __name__=="__main__":
     asyncio.run(main())
 
